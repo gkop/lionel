acd0a2043e2bf1a5baa2e1e00503426642c27608
Gabe Kopley <gabe@coshx.com>
2012-12-21 17:32:54 -0800

    Serve and build images from engines in middleman

--- lib/lionel/extensions/middleman.rb
+++ lib/lionel/extensions/middleman.rb
@@ -1,29 +1,34 @@
 module Lionel
   module Extensions
     module Middleman
+      class Options < Struct.new(:engines_with_images); end
+
       class << self
-        def registered(app)
+        def registered(app, &block)
+          options = Options.new([])
+          yield options if block_given?
           # Let's run our images/ through sprockets just as we do js and css,
           # so we can serve images packaged in engines
 
           # Inspired by https://github.com/Vasfed/middleman/commit/c9b4edcc0a3fb9fd4da43af78cedd50351479205
           # This may be CRUFT if similar behavior is implemented by middleman
           # itself
+          try_paths = [
+            %w{ assets },
+            %w{ app },
+            %w{ app assets },
+            %w{ vendor },
+            %w{ vendor assets },
+            %w{ lib },
+            %w{ lib assets }
+          ].inject([]) do |sum, v|
+            sum + [File.join(v, 'images')]
+          end
 
           app.after_configuration do
-            try_paths = [
-              %w{ assets },
-              %w{ app },
-              %w{ app assets },
-              %w{ vendor },
-              %w{ vendor assets },
-              %w{ lib },
-              %w{ lib assets }
-            ].inject([]) do |sum, v|
-              sum + [File.join(v, 'images')]
-            end
-
-            ([root] + ::Middleman.rubygems_latest_specs.map(&:full_gem_path)).each do |root_path|
+            ::Middleman.rubygems_latest_specs.select { |g|
+              options.engines_with_images.include?(g.name)
+            }.map(&:full_gem_path).each do |root_path|
               try_paths.map {|p| File.join(root_path, p) }.
                 select {|p| File.directory?(p) }.
                 each {|path| sprockets.append_path(path) }
@@ -36,7 +41,35 @@ module Lionel
             our_sprockets = sprockets
             map("/#{images_dir}")  { run our_sprockets }
           end
+
+          app.build_config do
+            # Copy over engined images to build/images/
+            FileUtils.mkdir_p "#{root}/build/images"
+            ::Middleman.rubygems_latest_specs.select { |g|
+              options.engines_with_images.include?(g.name)
+            }.map(&:full_gem_path).each do |root_path|
+              try_paths.map {|p| File.join(root_path, p) }.
+                select {|p| File.directory?(p) }.
+                each do |path|
+                  FileUtils.cp_r Dir[path+"/*"], "#{root}/build/images"
+              end
+            end
+          end
+
+          # ugly hack around current_path being nil when executing path helpers
+          if defined? ::Middleman::Extensions::RelativeAssets
+            ::Middleman::Extensions::RelativeAssets::InstanceMethods.module_eval do
+              alias_method :old_asset_url, :asset_url
+
+              def asset_url(path, prefix="")
+                old_asset_url(path, prefix)
+              rescue Exception => ex
+                "#{prefix}/#{path}"
+              end
+            end
+          end
         end
+
         alias :included :registered
       end
     end
